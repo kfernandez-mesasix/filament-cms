f8f81b516270c5ddb68f8750693b0ca08afa2b95
Kenneth Fernandez <kenneth@mesasix.com>
2024-12-09 19:27:04 +0800

    feat(settings): add navigation group settings, and update admin panel color settings

--- app/Filament/Pages/ManageSettings.php
+++ app/Filament/Pages/ManageSettings.php
@@ -12,6 +12,8 @@ class ManageSettings extends SettingsPage
 {
     protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
 
+    protected static ?string $navigationGroup = 'Settings';
+
     protected static string $settings = GeneralSettings::class;
 
     public function form(Form $form): Form
