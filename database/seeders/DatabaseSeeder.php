4eddc631d2ad47190bc84210c9c6f4eef544edbd
Kenneth Fernandez <kenneth@mesasix.com>
2024-12-12 22:39:53 +0800

    feat(database): update seeder to create user, super_admin role and assign all permissions to the user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
@@ -17,12 +18,28 @@ class DatabaseSeeder extends Seeder
         // Clear images
         Storage::deleteDirectory('public');
 
-        // Admin Creation
+        // Create Admin User
         $this->command->warn(PHP_EOL . 'Creating admin user...');
-        User::factory()->create([
+        $adminUser = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
         $this->command->info('Admin user created.');
+
+        // Create Super Admin Role
+        $this->command->warn(PHP_EOL . 'Creating super_admin role...');
+        $superAdminRole = Role::create(['name' => 'super_admin']);
+        $this->command->info('Super_admin role created.');
+
+        // Assign All Permissions to Super Admin
+        $this->command->warn(PHP_EOL . 'Assigning all permissions to super_admin role...');
+        $permissions = Permission::all();
+        $superAdminRole->syncPermissions($permissions);
+        $this->command->info('All permissions assigned to super_admin role.');
+
+        // Assign Role to Admin User
+        $this->command->warn(PHP_EOL . 'Assigning super_admin role to test@example.com...');
+        $adminUser->assignRole($superAdminRole);
+        $this->command->info('Super_admin role assigned to test@example.com.');
     }
 }
